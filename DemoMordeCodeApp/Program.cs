26f3dbebb3ba68e6d4f1da6b2204ecdf475072d7
Daniel Barrios <danielbarrios76@gmail.com>
2018-10-15 17:53:41 -0300

    [Issue2] fix code errors

--- DemoMordeCodeApp/Program.cs
+++ DemoMordeCodeApp/Program.cs
@@ -9,8 +9,8 @@ namespace DemoMordeCodeApp
         {
             var mc = new MorseCodeKey();
             string msgABC = "este mensaje es para codificar y ver el resultado";
-            string msgMorse = @"\.\...\-\.\\--\.\-.\...\.-\.---\.\\.\...\\.--.\.-\.-.\.-\\-..\.\-.-.\---\-..\..\..-.\..\-.-.\.-\.-.\\-.--\\...-\.\.-.\\.\.-..\\.-.\.\...\..-\.-..\-\.-\-..\---\";
-
+            //string msgMorse = @".\...\-\.\\--\.\-.\...\.-\.---\.\\.\...\\.--.\.-\.-.\.-\\-..\.\-.-.\---\-..\..\..-.\..\-.-.\.-\.-.\\-.--\\...-\.\.-.\\.\.-..\\.-.\.\...\..-\.-..\-\.-\-..\---\";
+            string msgMorse = @".----\---..";
             Console.WriteLine(msgABC);
             Console.WriteLine(mc.GetCodeMessage(msgABC));
             Console.ReadKey();
